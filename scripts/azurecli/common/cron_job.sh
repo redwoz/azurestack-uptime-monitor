ddcf312eec56151bbefc167b47db79c6ae69ebb1
Marc van Eijk <marcvaneijk@msn.com>
2019-01-02 22:59:37 +0100

    sync

---
+++ scripts/azurecli/common/cron_job.sh
@@ -0,0 +1,57 @@
+echo "=========== Job arguments"
+JOB_NAME=$1
+JOB_SCRIPT=$2
+JOB_TIMESTAMP=$(date --utc +%s%N)
+
+function azmon_existing_service_remove
+{
+    # Is there an existing service?
+    sudo docker service inspect $JOB_NAME > /dev/null \
+      && echo "Service $JOB_NAME exists" \
+      || { echo "There is no service with the name $JOB_NAME" ; return 0 ; }
+
+    # Get the timestamp from the existing serivce 
+    EXISTING_SERVICE_TIMESTAMP=$(sudo docker service inspect $JOB_NAME --format='{{.Spec.Labels.timestamp}}') \
+      && echo "Retrieved timestamp from existing service" \
+      || echo "Unable to retrieve timestamp from existing service"
+
+    # Check if existing serivce task is still running 
+    EXISTING_SERVICE_TASK_STATUS=$(sudo docker service ps $JOB_NAME --format "{{.CurrentState}}") \
+      && echo "Retrieved Docker Service task status" \
+      || echo "Unable to retrieve Docker Service task status"
+
+    # If existing service task is still running, update record in DB (based on existing TIMESTAMP)
+    [[ $TASK_STATUS == Running* ]] \
+      && curl -i -XPOST "http://localhost:8086/write?db=azmon" --data-binary "$JOB_NAME job=0,job_status=exceeded_max_runtime,job_runtime=$(( ($JOB_TIMESTAMP-$EXISTING_SERVICE_TIMESTAMP)/60000000000 )) $EXISTING_SERVICE_TIMESTAMP" \
+      || echo "Service task has exited"
+
+    # Remove the existing service
+    sudo docker service rm $JOB_NAME \
+    && echo "Removed existing service" \
+    || { echo "Unable to remove the existing service" ; return 1 ; }
+    
+    return 0
+}
+
+azmon_existing_service_remove
+# If the function exits with a 1, then the service exists but is not removed.
+# IF [ $? = 1 ] then notify?
+
+# Create new service and add a new record in the DB (with current JOB_TIMESTAMP)
+sudo docker service create \
+     --name $JOB_NAME \
+     --label timestamp=$JOB_TIMESTAMP \
+     --detach \
+     --restart-condition none \
+     --network="azmon" \
+     --mount type=bind,src=/azmon/azurecli,dst=/azmon/azurecli \
+     --env JOB_NAME=$JOB_NAME \
+     --env JOB_TIMESTAMP=$JOB_TIMESTAMP \
+     --secret fqdn \
+     --secret subscription_Id \
+     --secret app_Id \
+     --secret app_Key \
+     microsoft/azure-cli \
+     $JOB_SCRIPT \
+  && curl -i -XPOST "http://localhost:8086/write?db=azmon" --data-binary "$JOB_NAME job=0,job_status=created $JOB_TIMESTAMP" \
+  || echo "Unable to create service"
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,1 +1,112 @@
-echo $1
+echo "============== Set Argument Object ..."
+ARGUMENTS_JSON=$1
+
+###################################################
+#######   Requires Internet Connectivity   ########
+###################################################
+
+echo "============== Installing Prerequisistes ..."
+
+#update your existing list of packages
+sudo apt update
+
+# prerequisite packages which let apt use packages over HTTPS
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common jq
+
+# add the GPG key for the official Docker repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker repository to APT sources:
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Update package database with Docker packages from new repo
+sudo apt-get update
+
+# Install Docker
+sudo apt install -y docker-ce
+	
+echo "=========== Donwload docker images ..."
+
+sudo docker pull influxdb
+sudo docker pull grafana/grafana
+sudo docker pull microsoft/azure-cli
+
+####################################################
+######   No Internet Connectivity Required   #######
+####################################################
+
+BASE_URI=$(echo $ARGUMENTS_JSON | jq -r ".baseUrl")
+FQDN=$(echo $ARGUMENTS_JSON | jq -r ".fqdn")
+SUBSCRIPTION_ID=$(echo $ARGUMENTS_JSON | jq -r ".subscriptionId")
+APP_ID=$(echo $ARGUMENTS_JSON | jq -r ".appId")
+APP_KEY=$(echo $ARGUMENTS_JSON | jq -r ".appKey")
+TENANT_NAME=$(echo $ARGUMENTS_JSON | jq -r ".tenantName")
+GRAFANA_ADMIN=$(echo $ARGUMENTS_JSON | jq -r ".grafanaPassword")
+
+# Create a directory structure for the project
+sudo mkdir /azmon
+sudo mkdir /azmon/azurecli
+sudo mkdir /azmon/azurecli/jobs
+sudo mkdir /azmon/azurecli/common
+sudo mkdir /azmon/influxdb
+sudo mkdir /azmon/grafana
+sudo mkdir /azmon/grafana/datasources
+sudo mkdir /azmon/grafana/dashboards
+
+# Download the files
+sudo curl -s "$BASE_URI"/scripts/azurecli/common/auth.sh --output /azmon/azurecli/common/auth.sh
+sudo curl -s "$BASE_URI"/scripts/azurecli/common/cron_job.sh --output /azmon/azurecli/common/cron_job.sh
+sudo curl -s "$BASE_URI"/scripts/azurecli/common/cron_tab.txt --output /azmon/azurecli/common/cron_tab.txt
+sudo curl -s "$BASE_URI"/scripts/azurecli/jobs/pnu.sh --output /azmon/azurecli/jobs/pnu.sh
+sudo curl -s "$BASE_URI"/scripts/grafana/dashboards/azmon.json --output /azmon/grafana/dashboards/azmon.json
+sudo curl -s "$BASE_URI"/scripts/grafana/datasources/influxdb.yml --output /azmon/grafana/datasources/influxdb.yml
+
+# Copy the waagent cert to the project folder
+sudo cp /var/lib/waagent/Certificates.pem /azmon/azurecli/common/Certificates.pem
+
+# change the permissions for all files in /azmon/azurecli 
+sudo chmod -R 755 /azmon/azurecli
+
+echo "=========== Initialize Docker Swarm ..."
+
+# Install Docker Swarm
+sudo docker swarm init
+
+echo "=========== Create Docker Secrets ..."
+
+# Create the secrets
+printf $FQDN | sudo docker secret create fqdn -
+printf $SUBSCRIPTION_ID | sudo docker secret create subscription_Id -
+printf $APP_ID | sudo docker secret create app_Id -
+printf $APP_KEY | sudo docker secret create app_Key -
+printf $GRAFANA_ADMIN | sudo docker secret create grafana_Admin -
+
+echo "=========== Create network"
+#sudo docker network ls
+sudo docker network create --driver overlay "azmon"
+
+echo "=========== Start InfluxDB container"
+sudo docker service create \
+     --name influxdb \
+     --detach \
+     --restart-condition any \
+     --network="azmon" \
+     --mount type=bind,src=/azmon/influxdb,dst=/var/lib/influxdb \
+     --publish published=8086,target=8086 \
+     --env INFLUXDB_DB=azmon \
+     influxdb
+
+echo "=========== Start Grafana container"
+sudo docker service create \
+     --name grafana \
+     --detach \
+     --restart-condition any \
+     --network="azmon" \
+     --mount type=bind,src=/azmon/grafana,dst=/etc/grafana/provisioning \
+     --publish published=3000,target=3000 \
+     --secret grafana_Admin \
+     --env GF_SECURITY_ADMIN_PASSWORD__FILE=/run/secrets/grafana_Admin \
+     grafana/grafana
+
+echo "=========== Configure cron"
+sudo crontab -u $(whoami) /azmon/azurecli/common/cron_tab.txt
