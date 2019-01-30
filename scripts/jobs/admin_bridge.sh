4cbfbbfa5924d49a9c3ec4d1ecf158066ae68b07
Marc van Eijk <marcvaneijk@msn.com>
2019-01-30 14:47:00 +0100

    update error handling

--- scripts/jobs/admin_bridge.sh
+++ scripts/jobs/admin_bridge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION=0.4
+SCRIPT_VERSION=0.4s
 
 # Source functions.sh
 source /azs/common/functions.sh \
@@ -22,32 +22,34 @@ azs_task_start read
 BRIDGE_ACTIVATION_ID=$(az resource list \
       --resource-type "Microsoft.AzureBridge.Admin/activations" \
       | jq -r ".[0].id") \
-  && azs_log_field T status admin_bridge_update_location_id \
-  || azs_log_field T status admin_pnu_update_location_id fail
+  && azs_log_field T status admin_bridge_activation_id \
+  || azs_log_field T status admin_bridge_activation_id fail
 
 # Get update location status
 BRIDGE_REGISTRATION_ID=$(az resource show \
       --ids $BRIDGE_ACTIVATION_ID \
       | jq -r ".properties.azureRegistrationResourceIdentifier") \
-  && azs_log_field T status admin_pnu_update_location_status \
-  || azs_log_field T status admin_pnu_update_location_status fail
+  && azs_log_field T status admin_bridge_registration_id \
+  || azs_log_field T status admin_bridge_registration_id fail
 
 # Remove leading "/"
 BRIDGE_SUBSCRIPTION_ID=${BRIDGE_REGISTRATION_ID#*/} \
-  && azs_log_field T status admin_pnu_update_location_status \
-  || azs_log_field T status admin_pnu_update_location_status fail
+  && azs_log_field T status admin_bridge_substring_01 \
+  || azs_log_field T status admin_bridge_substring_01 fail
 
 # Remove "sbscriptions/"
 BRIDGE_SUBSCRIPTION_ID=${BRIDGE_SUBSCRIPTION_ID#*/} \
-  && azs_log_field T status admin_pnu_update_location_status \
-  || azs_log_field T status admin_pnu_update_location_status fail
+  && azs_log_field T status admin_bridge_substring_02 \
+  || azs_log_field T status admin_bridge_substring_02 fail
 
 # Remove traling path from subscription id
 BRIDGE_SUBSCRIPTION_ID=${BRIDGE_SUBSCRIPTION_ID%%/*} \
-  && azs_log_field T status admin_pnu_update_location_status \
-  || azs_log_field T status admin_pnu_update_location_status fail
+  && azs_log_field T status admin_bridge_substring_03 \
+  || azs_log_field T status admin_bridge_substring_03 fail
 
-echo $BRIDGE_SUBSCRIPTION_ID > /azs/bridge/subscriptionid
+echo $BRIDGE_SUBSCRIPTION_ID > /azs/bridge/subscriptionid \
+  && azs_log_field T status admin_bridge_to_file \
+  || azs_log_field T status admin_bridge_to_file fail
 
 azs_task_end read
 ############################### Job: Complete #################################
