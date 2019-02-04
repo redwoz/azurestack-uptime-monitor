387fbfa72670543f116fdc56eaa086ffa5403051
Marc van Eijk <marcvaneijk@msn.com>
2019-02-04 22:44:16 +0100

    add linked template for read resources

--- scripts/jobs/srv_deploy_template.sh
+++ scripts/jobs/srv_deploy_template.sh
@@ -15,13 +15,24 @@ azs_task_start auth
 azs_login management
 
 azs_task_end auth
-################################## Task: Read #################################
-azs_task_start read
+################################# Task: Create ################################
+azs_task_start create
 
-RESOURCES=$(az resource list) \
-  && azs_log_field T status tenant_storage_read \
-  || azs_log_field T status tenant_storage_read fail
+# Get location from FQDN
+FQDN=$(cat /run/secrets/fqdn)
+LOCATION=${FQDN%%.*}
 
-azs_task_end read
+# Create the resource group if it doesn't already exist
+az group create \
+  --location $LOCATION \
+  --name $(cat /run/secrets/uniqueString)
+
+az group deployment create \
+  --resource-group $(cat /run/secrets/uniqueString) \
+  --name bootstrap \
+  --template-uri "$(cat /run/secrets/baseUrl)"/linked/readTemplate.json
+  --parameters uniqueString=$(cat /run/secrets/uniqueString)
+
+azs_task_end create
 ############################### Job: Complete #################################
 azs_job_end
