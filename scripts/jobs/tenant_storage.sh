cb33c4b31aa9c97585bde5ced3a369c6225db720
Marc van Eijk <marcvaneijk@msn.com>
2019-02-07 19:12:47 +0100

    read content from file in blob

--- scripts/jobs/tenant_storage.sh
+++ scripts/jobs/tenant_storage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION=0.5
+SCRIPT_VERSION=0.6
 
 # Source functions.sh
 source /azs/common/functions.sh \
@@ -10,9 +10,15 @@ source /azs/common/functions.sh \
 azs_task_start read
 
 STORAGE_ACCOUNT_NAME="$(cat /run/secrets/uniqueString)"storage
-BLOB_ENDPOINT=".blob.${FQDN}"
+BLOB_ENDPOINT=.blob."$(cat /run/secrets/fqdn)"
 
-echo $(curl https://${STORAGE_ACCOUNT_NAME}${BLOB_ENDPOINT}/test/read.log)
+FILE_CONTENT=$(curl https://${STORAGE_ACCOUNT_NAME}${BLOB_ENDPOINT}/test/read.log) \
+  && azs_log_field T status tenant_storage_read_file \
+  || azs_log_field T status tenant_storage_read_file fail
+
+[ "$FILE_CONTENT" = "$(cat /run/secrets/uniqueString)" ] \
+  && azs_log_field T status tenant_storage_compare_content \
+  || azs_log_field T status tenant_storage_compare_content fail
 
 azs_task_end read
 ############################### Job: Complete #################################
